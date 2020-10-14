7da55e77789deddd881a223f8583ee89a7f154ec
Jay <jmandavilli@gmail.com>
2020-10-13 23:23:25 -0600

    have draft for sampling function

--- GaborFilters/GaborRandomSampling.cpp
+++ GaborFilters/GaborRandomSampling.cpp
@@ -21,11 +21,30 @@ public:
 };
 
 std::vector<std::vector<double>> cpprinvgamma(int n, double shape, double scale) {
-  
+  std::default_random_engine generator;
+  std::gamma_distribution<double> distribution(shape, scale);
+  std::vector<double> out;
+
+  // Could do more efficiently than using push_back
+  for(int i=0; i < n; ++i) {
+    double number = 1/distribution(generator);
+    out.push_back(number);
+  }
+  return out;      
 }
 
 std::vector<std::vector<double>> cpprunif(int n, double min, double max) {
+  std::default_random_engine generator;
+  std::uniform_real_distribution<double> distribution(min, max);
+  std::vector<double> out;
+  double mean = (min + max)/2;
+  double std = sqrt((pow(max-min, 2))/12);
+  for(int i=0; i < n; ++i) {
+    double number = (distribution(generator) - mean) / std;
+    out.push_back(number); 
+  }
 
+  return out;
 }
 
 Random GaborRandomSampling(int n=1, double galpha=3, double gbeta=1, double igalpha=1, double igbeta=0.5, double iw) {
@@ -38,4 +57,6 @@ Random GaborRandomSampling(int n=1, double galpha=3, double gbeta=1, double igal
   sampling.x0 = cpprunif(n, -2.5, 2.5);
   sampling.y0 = cpprunif(n, -2.5, 2.5);
   sampling.tau = cpprunif(n, 0, 2 * boost::math::constants::pi<T>());
+
+  return sampling;
 }
